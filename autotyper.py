4a2161258d3649a95b846ee6e34ff5a76e2081e4
Asbjørn <65400862+AsbjornLind@users.noreply.github.com>
2021-06-29 13:52:51 +0200

    feat : While loop, for multiple assignments

--- autotyper.py
+++ autotyper.py
@@ -1,30 +1,62 @@
 from pynput.keyboard import Key, Controller
 import time
 
-formular = input("""
-indtast den formular du vil have skrevet ind, uden tallet til sidst. eksempelvis: 
-jeg vil have skrevet tal ind fra 1-100 af  "=JDK!A23" 
-skriv så kun "=JDK!A" uden anførselstegn.
-""")
 
-start_tal = int(input("""Hvilket tal skal jeg starte fra? I eksemplet vil det være 1./n
-"""))
-print("---------------")
-slut_tal = int(input("""hvilket række er den sidste? I eksemplet er det 100.
-"""))
+print("--------------------------------------")
+print("--------------------------------------")
+print("   PRESS ENTER TO START OR Q TO QUIT")
+print("--------------------------------------")
+print("--------------------------------------")
+decission = input("")
 
-waiter = int(input("Hvor mange sekunder vil du vente før jeg starter?" ))
-print(f"""Du har nu {waiter} sekunder til at klikke i den celle jeg skal starte i! :)  
-SKYND DIG! """)
+while decission.lower() != 'q':
+    formular = input("""indtast den formular du vil have skrevet ind, uden tallet til sidst. eksempelvis: 
+    jeg vil have skrevet tal ind fra 1-100 af  "=JDK!A23" 
+    skriv så kun "=JDK!A" uden anførselstegn.
+    """)
+    print("---------------")
 
-time.sleep(waiter)
-keyboard = Controller()
-for i in range(start_tal,slut_tal+1):
-    cell_value = f"{formular}{i}"
+    start_tal = int(input("""Hvilket tal skal jeg starte fra? I eksemplet vil det være 1.
+    """))
+    print("---------------")
 
-    for letters in cell_value:
-        keyboard.type(letters)
+    slut_tal = int(input("""hvilket række er den sidste? I eksemplet er det 100.
+    """))
+    print("---------------")
 
-    time.sleep(0.15)
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
+    gentagelse = int(input("""Hvor mange tv stationer har hver kampagne?
+    """))
+    print("---------------")
+
+    waiter = int(input("Hvor mange sekunder vil du vente før jeg starter?" ))
+    print(f"""Du har nu {waiter} sekunder til at klikke i den celle jeg skal starte i! :)  
+    SKYND DIG! """)
+
+    time.sleep(waiter)
+    keyboard = Controller()
+    for i in range(start_tal,slut_tal+1):
+        for _ in range(0,gentagelse):
+            cell_value = f"{formular}{i}"
+
+            for letters in cell_value:
+                keyboard.type(letters)
+
+            time.sleep(0.15)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+    
+    print("     Skud ud til Asbjørn")
+    time.sleep(2)
+    print("--------------------------------------")
+    print("--------------------------------------")
+    print("   PRESS ENTER TO START OR Q TO QUIT")
+    print("--------------------------------------")
+    print("--------------------------------------")
+    decission = input("")
+
+
+
+
+
+##pynput skal være rigtig version for det virker til pyinstaller
+#https://stackoverflow.com/questions/63681770/getting-error-when-using-pynput-with-pyinstaller
